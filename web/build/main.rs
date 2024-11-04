542c36d0ab8ada8480bae06bd9995637662b8856
tipsypastels <dakotasankey@gmail.com>
2024-11-04 13:35:03 -0800

    tailwind in build

--- web/build/main.rs
+++ web/build/main.rs
@@ -1,4 +1,6 @@
 use anyhow::Result;
+use camino::Utf8PathBuf;
+use std::env;
 
 mod artifact;
 mod binary;
@@ -8,6 +10,10 @@ mod tailwind;
 async fn main() -> Result<()> {
     artifact::init().await?;
     binary::init().await?;
+
+    let out_dir: Utf8PathBuf = env::var("OUT_DIR").unwrap().into();
+
+    tailwind::build(&out_dir).await?;
 
     Ok(())
 }
--- web/build/tailwind.rs
+++ web/build/tailwind.rs
@@ -1,1 +1,23 @@
+use crate::binary::TAILWIND;
+use anyhow::{ensure, Result};
+use camino::Utf8Path;
+use tokio::process::Command;
 
+pub async fn build(out_dir: &Utf8Path) -> Result<()> {
+    let output = out_dir.join("tailwind.css");
+    let binary = TAILWIND.path().await?;
+    let mut command = Command::new(binary.as_str());
+
+    command
+        .kill_on_drop(true)
+        .args(["--config", "tailwind.config.js"])
+        .args(["--input", "tailwind.css"])
+        .args(["--output", output.as_str()])
+        .arg("--minify");
+
+    let status = command.status().await?;
+    let success = status.success();
+
+    ensure!(success, "tailwind failed, status {status}");
+    Ok(())
+}
