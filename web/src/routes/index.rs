1febea82e292e200105f0b0f58fe2011b6f4e9db
tipsypastels <dakotasankey@gmail.com>
2024-11-05 19:38:07 -0800

    add CellIter

--- web/src/routes/index.rs
+++ web/src/routes/index.rs
@@ -1,17 +1,20 @@
-use crate::{grimoire::GrimoireLock, render::fa};
+use crate::{
+    grimoire::GrimoireLock,
+    render::{fa, CellIter},
+};
 use askama_axum::Template;
 use axum::response::{IntoResponse, Response};
-use grimoire_core::Node;
+use grimoire_core::Nodes;
 
 pub async fn get(grimoire: GrimoireLock) -> Response {
     #[derive(Template)]
     #[template(path = "index.html", escape = "none")]
     pub struct IndexHtml<'a> {
-        nodes: Vec<&'a Node>,
+        nodes: CellIter<Nodes<'a>>,
     }
 
     let grimoire = grimoire.read().await;
-    let nodes = grimoire.nodes().collect();
+    let nodes = grimoire.nodes().into();
     let template = IndexHtml { nodes };
 
     template.into_response()
