62f783cbe630e43e5b58d0544eb4d9a2b82384ae
tipsypastels <dakotasankey@gmail.com>
2024-11-06 19:50:12 -0800

    add swimsuit palette

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   darkMode: "selector",
   content: ["./templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        swimsuit: "#2f2bad",
+        spraypaint: "#ad2bad",
+        sweetness: "#e42692",
+        sexercise: "#f71568",
+        solidgold: "#f9da4b",
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
